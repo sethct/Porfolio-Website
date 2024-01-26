5d25da6750c9bba723e318444f8ba9f4826b0e8e
Seth <45259387+sethct@users.noreply.github.com>
2024-01-26 14:09:03 +0000

    semi-functioning dark mode. need to adjust responsive spacing of toggle and colour of specific elements.

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleSwitch = document.getElementById('dark-mode-toggle');
+    const body = document.body;
+
+    // Check if a dark mode preference is stored
+    if (localStorage.getItem('darkMode') == 'on') {
+        body.classList.add('dark-mode');
+        toggleSwitch.checked = true;
+    }
+
+    // Listen for a change on the toggle switch
+    toggleSwitch.addEventListener('change', function () {
+        if (this.checked) {
+            body.classList.add('dark-mode');
+            localStorage.setItem('darkMode', 'on');
+        } else {
+            body.classList.remove('dark-mode');
+            localStorage.setItem('darkMode', 'off');
+        }
+    });
+})
